9a8933221c3712dec7c5a796e96a0fed155910d8
Carlos Rojas <carlosrb206@gmail.com>
2022-01-30 23:51:59 -0500

    fixed

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -70,7 +70,11 @@ const SearchBooks = () => {
 
     try {
       await saveBook({
-        variables: { book: bookToSave }
+        variables: { book: bookToSave },
+        update: cache => {
+          const {me} = cache.readQuery({ query: GET_ME });
+          cache.writeQuery({ query: GET_ME , data: {me: { ...me, savedBooks: [...me.savedBooks, bookToSave] } } })
+        }
       });
 
       // if book successfully saves to user's account, save book id to state
